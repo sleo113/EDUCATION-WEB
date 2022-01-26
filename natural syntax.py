f1a5ffbe35eeefffc6575b7eaee5804071998c33
JeUk Park <77144006+sleo113@users.noreply.github.com>
2022-01-26 16:14:46 +0900

    Add files via upload

---
+++ natural syntax.py
@@ -0,0 +1,39 @@
+# first_name = ' je uk'
+# last_name = 'park'
+# num = str(2)
+# print(first_name+ num)
+
+# a_list = ['사과','배','감']
+
+# def sum(num1,num2):
+#     print("안녕!")
+#     return num1+num2
+#
+# result = sum(2,3)
+#
+# print(result)
+
+# def is_adult(age):
+#     if age > 20:
+#         print("성인입니다")
+#     else:
+#         print("청소년입니다")
+
+# fruits = ['사과','배','배','감','수박','귤','딸기','사과','배','수박']
+#
+# count = 0
+# for fruit in fruits:
+# 	if fruit == '사과':
+# 		count += 1
+#
+# print(count)
+
+people = [{'name': 'bob', 'age': 20},
+          {'name': 'carry', 'age': 38},
+          {'name': 'john', 'age': 7},
+          {'name': 'smith', 'age': 17},
+          {'name': 'ben', 'age': 27}]
+
+for person in people :
+    print(person['name'],person['age'])
+
